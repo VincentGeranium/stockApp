6e2e433ba41b936c99b845ffa0c327693ce14835
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-04-16 11:30:21 +0900

    feat: setted up search controller.

--- stockApp/stockApp/Controllers/WatchListViewController.swift
+++ stockApp/stockApp/Controllers/WatchListViewController.swift
@@ -13,5 +13,35 @@ class WatchListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
+        setupSearchController()
+    }
+}
+
+// MARK: - Private
+extension WatchListViewController {
+    private func setupSearchController() {
+        let searchResultsController = SearchResultsViewController()
+        let searchController = UISearchController(searchResultsController: searchResultsController)
+        searchController.searchResultsUpdater = self
+        self.navigationItem.searchController = searchController
+        
+    }
+}
+
+// MARK: - extension UISearchResultsUpdating
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let resultVC = searchController.searchResultsController as? SearchResultsViewController,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        // Optimize to reduce number of searches for when user stops typing
+        
+        // Call API to Search
+        
+        // Update result controllers
+        print(query)
     }
 }
